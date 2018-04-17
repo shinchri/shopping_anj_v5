8007d771c86fec6365c84c3622ecaa753342a469
Unknown <shinchri7@gmail.com>
2018-04-17 11:21:40 -0500

    implemented a feature to add to shopping list

--- Shopping-Proj/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Shopping-Proj/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
 
+  // emitting new event to parent; passing object; Output() so you can listen to the event outside
+  // ingredientAdded = new EventEmitter<{name: string, amount: number}>();
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  onAddItem() {
+    // declared const instead of let because this value will not change
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    // emitting new event
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
